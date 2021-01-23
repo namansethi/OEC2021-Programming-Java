2a844738055d5720ee6fafa6b8ef8895d47b09c1
Rhys <rhys.melanson@yahoo.com>
2021-01-23 18:31:55 -0500

    TAs and teachers get infected

--- src/ZBY1_Tracker.py
+++ src/ZBY1_Tracker.py
@@ -30,9 +30,9 @@ def initializeTransmissionRates():
 
 def assignClassInfectionRates(period, className):
 
-    print(period,className)
+
     course_and_period = studentRecords.loc[studentRecords[period] == className]
-    print(course_and_period)
+
 
     infected = course_and_period.loc[course_and_period['Infection Rate'] != 'NaN']
 
@@ -50,23 +50,23 @@ def assignClassInfectionRates(period, className):
 
     grad9 = infected.loc[infected['Grade'] == 9]
     col_one_list = grad9['Infection Rate'].tolist()
-    multiplied_list = [element / 100 * (3 / num) for element in col_one_list]
-    grade9infect = calc_infect_rate(col_one_list)
+    multiplied_list = [(element / 100) * (3 / num) for element in col_one_list]
+    grade9infect = calc_infect_rate(multiplied_list)
 
     grad10 = infected.loc[infected['Grade'] == 10]
     col_one_list = grad10['Infection Rate'].tolist()
-    multiplied_list = [element / 100 * (3 / num) for element in col_one_list]
-    grade10infect = calc_infect_rate(col_one_list)
+    multiplied_list = [(element / 100) * (3 / num) for element in col_one_list]
+    grade10infect = calc_infect_rate(multiplied_list )
 
     grad11 = infected.loc[infected['Grade'] == 11]
     col_one_list = grad11['Infection Rate'].tolist()
-    multiplied_list = [element / 100 * (3 / num) for element in col_one_list]
-    grade11infect = calc_infect_rate(col_one_list)
+    multiplied_list = [(element / 100) * (3 / num) for element in col_one_list]
+    grade11infect = calc_infect_rate(multiplied_list)
 
     grad12 = infected.loc[infected['Grade'] == 12]
     col_one_list = grad12['Infection Rate'].tolist()
-    multiplied_list = [element / 100 * (3 / num) for element in col_one_list]
-    grade12infect = calc_infect_rate(col_one_list)
+    multiplied_list = [(element / 100) * (3 / num) for element in col_one_list]
+    grade12infect = calc_infect_rate(multiplied_list)
 
     if (teacher.empty):
         teachInfect = 0
@@ -76,9 +76,9 @@ def assignClassInfectionRates(period, className):
         TaInfect = 0
     else:
         TaInfect = int(ta['Infection Rate'])
-    isClassRoomInfected = 'false'
 
-    infectRate = 0.5
+
+
 
     for index, row in course_and_period.iterrows():
 
@@ -86,30 +86,34 @@ def assignClassInfectionRates(period, className):
         studNum = row['Student Number']
 
         healthcondition = row['Health Conditions']
-        infection = row['Infection Rate']
+        infection = row['Infection Rate']/100
 
         if (healthcondition != 'nan'):
             healthcondition = 'true'
+
         if (infection == 1):
             continue
+
         if grade == 9:
             if healthcondition:
                 list = [grade9infect, grade10infect, grade11infect, grade12infect]
                 infect = calc_infect_rate(list)
                 infect = infect * 1.7
+
                 if (infect >= 1):
                     infection = 1
                 else:
                     infection = 1 - (1 - infection) * (1 - infect)
             else:
                 list = [infection, grade9infect, grade10infect, grade11infect, grade12infect]
-                infection = calc_infect_rate(list)
+                infection = 1 - (1 - infection) * (1-calc_infect_rate(list))
 
         if grade == 10:
             if healthcondition:
                 list = [grade9infect * 1.25, grade10infect, grade11infect, grade12infect]
                 infect = calc_infect_rate(list)
                 infect = infect * 1.7
+
                 if (infect >= 1):
                     infection = 1
                 else:
@@ -117,6 +121,7 @@ def assignClassInfectionRates(period, className):
             else:
                 list = [grade9infect * 1.25, grade10infect, grade11infect, grade12infect]
                 infect = calc_infect_rate(list)
+
                 if (infect >= 1):
                     infection = 1
                 else:
@@ -127,6 +132,7 @@ def assignClassInfectionRates(period, className):
                 list = [grade9infect * 1.5, grade10infect * 1.25, grade11infect, grade12infect]
                 infect = calc_infect_rate(list)
                 infect = infect * 1.7
+
                 if (infect >= 1):
                     infection = 1
                 else:
@@ -134,6 +140,7 @@ def assignClassInfectionRates(period, className):
             else:
                 list = [grade9infect * 1.5, grade10infect * 1.25, grade11infect, grade12infect]
                 infect = calc_infect_rate(list)
+
                 if (infect >= 1):
                     infection = 1
                 else:
@@ -144,6 +151,7 @@ def assignClassInfectionRates(period, className):
                 list = [grade9infect * 1.75, grade10infect * 1.5, grade11infect * 1.25, grade12infect]
                 infect = calc_infect_rate(list)
                 infect = infect * 1.7
+
                 if (infect >= 1):
                     infection = 1
                 else:
@@ -151,20 +159,45 @@ def assignClassInfectionRates(period, className):
             else:
                 list = [grade9infect * 1.75, grade10infect * 1.5, grade11infect * 1.25, grade12infect]
                 infect = calc_infect_rate(list)
+
                 if (infect >= 1):
                     infection = 1
                 else:
                     infection = 1 - (1 - infection) * (1 - infect)
         infection = 1 - (1 - infection) * (1 - 3 / num * teachInfect * 0.2)
         infection = 1 - (1 - infection) * (1 - 3 / num * TaInfect * 0.2)
+
         # note working need to be changed
-        if isClassRoomInfected:
-            infection = infection + 3 / num * infectRate
 
-        studentRecords.loc[studentRecords['Student Number'] == studNum, 'Infection Rate'] = infection
 
-    print(infected)
-    print(grad10)
+        print(infection)
+
+        studentRecords.loc[studentRecords['Student Number'] == studNum, 'Infection Rate'] = infection*100
+
+
+    if(teacher.empty):
+        infection = 0
+    else:
+        teaching = teacher['Teacher Number']
+        infection = int(teacher['Infection Rate']) / 100
+        list = [grade9infect, grade10infect, grade11infect, grade12infect]
+        infection = (1- (1-infection)*(1-calc_infect_rate(list))) * 0.2
+        infection = 1 - (1 - infection) * (1 - 3 / num * TaInfect )
+
+        teacherRecords.loc[teacherRecords['Teacher Number'] == teaching.iloc[0], 'Infection Rate'] = infection*100
+
+    if(ta.empty):
+        infection = 0
+    else:
+        teaching = ta['First Name']
+        infection = int(ta['Infection Rate']) / 100
+        list = [grade9infect, grade10infect, grade11infect, grade12infect]
+        infection = (1 - (1 - infection) * (1 - calc_infect_rate(list))) * 0.2
+        infection = 1 - (1 - infection) * (1 - 3 / num * teachInfect)
+        taRecords.loc[taRecords['First Name'] == teaching.iloc[0], 'Infection Rate'] = infection * 100
+
+    #print(infected)
+   # print(grad10)
 
     pass
     #TODO where RHYS's code goes
