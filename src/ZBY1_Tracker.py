cbf4be7fd4407ddd35dc0958d01e4a68f64f6c8c
Naman Sethi <namansethi@cmail.carleton.ca>
2021-01-23 14:38:54 -0500

    Working on initial logic.

--- src/ZBY1_Tracker.py
+++ src/ZBY1_Tracker.py
@@ -1,6 +1,31 @@
+import pandas as pd
+import numpy as np
+
 # Import file with data loading functions
 
-studentRecords = 1
-teacherRecords = 2
-taRecords = 3
-infectedStatus = 4
+studentRecords = pd.read_excel('../Resources/OEC2021_-_School_Record_Book_.xlsx', sheet_name='Student Records')
+teacherRecords = pd.read_excel('../Resources/OEC2021_-_School_Record_Book_.xlsx', sheet_name='Teacher Records')
+taRecords = pd.read_excel('../Resources/OEC2021_-_School_Record_Book_.xlsx', sheet_name='Teaching Assistant Records')
+infectedStudents = pd.read_excel('../Resources/OEC2021_-_School_Record_Book_.xlsx', sheet_name='ZBY1 Status')
+
+
+def addInfectionColumn(dataframe):
+    dataframe["Infection Rate"] = "0"
+    pass
+
+
+def initializeTransmissionRates():
+    studentRecords.dropna()
+    infected_student_numbers = studentRecords.loc[studentRecords['Student Number'].isin(infectedStudents['Student ID'])]
+    studentRecords['Infection Rate'] = studentRecords['Student Number'].apply(lambda x: 100 if x in infected_student_numbers['Student Number'].values else 0)
+    print(studentRecords.to_string())
+    pass
+
+
+def startProgram():
+    addInfectionColumn(studentRecords)
+    initializeTransmissionRates()
+    pass
+
+
+startProgram()
