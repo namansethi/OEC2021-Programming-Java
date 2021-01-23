584bff0dbf86053cbafc0e81e25d1b939446de78
Rhys <rhys.melanson@yahoo.com>
2021-01-23 17:05:16 -0500

    Commit

--- src/ZBY1_Tracker.py
+++ src/ZBY1_Tracker.py
@@ -30,10 +30,153 @@ def initializeTransmissionRates():
 
 def assignClassInfectionRates(period, className):
 
+    print(period,className)
+    course_and_period = studentRecords.loc[studentRecords[period] == className]
+    print(course_and_period)
+
+    infected = course_and_period.loc[course_and_period['Infection Rate'] != 'NaN']
+
+    ta = taRecords.loc[taRecords[period] == className]
+
+    teacher = teacherRecords.loc[teacherRecords['Class'] == className]
+
+
+
+    if (ta.empty):
+        num = len(course_and_period) + 1
+
+    else:
+        num = len(course_and_period) + 2
+
+    grad9 = infected.loc[infected['Grade'] == 9]
+    col_one_list = grad9['Infection Rate'].tolist()
+    multiplied_list = [element / 100 * (3 / num) for element in col_one_list]
+    grade9infect = calc_infect_rate(col_one_list)
+
+    grad10 = infected.loc[infected['Grade'] == 10]
+    col_one_list = grad10['Infection Rate'].tolist()
+    multiplied_list = [element / 100 * (3 / num) for element in col_one_list]
+    grade10infect = calc_infect_rate(col_one_list)
+
+    grad11 = infected.loc[infected['Grade'] == 11]
+    col_one_list = grad11['Infection Rate'].tolist()
+    multiplied_list = [element / 100 * (3 / num) for element in col_one_list]
+    grade11infect = calc_infect_rate(col_one_list)
+
+    grad12 = infected.loc[infected['Grade'] == 12]
+    col_one_list = grad12['Infection Rate'].tolist()
+    multiplied_list = [element / 100 * (3 / num) for element in col_one_list]
+    grade12infect = calc_infect_rate(col_one_list)
+
+    if (teacher.empty):
+        teachInfect = 0
+    else:
+        teachInfect = int(teacher['Infection Rate'])
+    if (ta.empty):
+        TaInfect = 0
+    else:
+        TaInfect = int(ta['Infection Rate'])
+    isClassRoomInfected = 'false'
+
+    infectRate = 0.5
+
+    for index, row in course_and_period.iterrows():
+
+        grade = row['Grade']
+        studNum = row['Student Number']
+
+        healthcondition = row['Health Conditions']
+        infection = row['Infection Rate']
+
+        if (healthcondition != 'nan'):
+            healthcondition = 'true'
+        if (infection == 1):
+            continue
+        if grade == 9:
+            if healthcondition:
+                list = [grade9infect, grade10infect, grade11infect, grade12infect]
+                infect = calc_infect_rate(list)
+                infect = infect * 1.7
+                if (infect >= 1):
+                    infection = 1
+                else:
+                    infection = 1 - (1 - infection) * (1 - infect)
+            else:
+                list = [infection, grade9infect, grade10infect, grade11infect, grade12infect]
+                infection = calc_infect_rate(list)
+
+        if grade == 10:
+            if healthcondition:
+                list = [grade9infect * 1.25, grade10infect, grade11infect, grade12infect]
+                infect = calc_infect_rate(list)
+                infect = infect * 1.7
+                if (infect >= 1):
+                    infection = 1
+                else:
+                    infection = 1 - (1 - infection) * (1 - infect)
+            else:
+                list = [grade9infect * 1.25, grade10infect, grade11infect, grade12infect]
+                infect = calc_infect_rate(list)
+                if (infect >= 1):
+                    infection = 1
+                else:
+                    infection = 1 - (1 - infection) * (1 - infect)
+
+        if grade == 11:
+            if healthcondition:
+                list = [grade9infect * 1.5, grade10infect * 1.25, grade11infect, grade12infect]
+                infect = calc_infect_rate(list)
+                infect = infect * 1.7
+                if (infect >= 1):
+                    infection = 1
+                else:
+                    infection = 1 - (1 - infection) * (1 - infect)
+            else:
+                list = [grade9infect * 1.5, grade10infect * 1.25, grade11infect, grade12infect]
+                infect = calc_infect_rate(list)
+                if (infect >= 1):
+                    infection = 1
+                else:
+                    infection = 1 - (1 - infection) * (1 - infect)
+
+        if grade == 12:
+            if healthcondition:
+                list = [grade9infect * 1.75, grade10infect * 1.5, grade11infect * 1.25, grade12infect]
+                infect = calc_infect_rate(list)
+                infect = infect * 1.7
+                if (infect >= 1):
+                    infection = 1
+                else:
+                    infection = 1 - (1 - infection) * (1 - infect)
+            else:
+                list = [grade9infect * 1.75, grade10infect * 1.5, grade11infect * 1.25, grade12infect]
+                infect = calc_infect_rate(list)
+                if (infect >= 1):
+                    infection = 1
+                else:
+                    infection = 1 - (1 - infection) * (1 - infect)
+        infection = 1 - (1 - infection) * (1 - 3 / num * teachInfect * 0.2)
+        infection = 1 - (1 - infection) * (1 - 3 / num * TaInfect * 0.2)
+        # note working need to be changed
+        if isClassRoomInfected:
+            infection = infection + 3 / num * infectRate
+
+        studentRecords.loc[studentRecords['Student Number'] == studNum, 'Infection Rate'] = infection
+
+    print(infected)
+    print(grad10)
+
+    pass
     #TODO where RHYS's code goes
 
     pass
 
+def calc_infect_rate(list_of_rates):
+    intermediate_rate = 1
+    for rate in list_of_rates:
+        intermediate_rate = intermediate_rate * (1 - rate)
+
+    return 1 - intermediate_rate
 
 def startProgram():
     addInfectionColumn(studentRecords)
@@ -44,7 +187,7 @@ def startProgram():
     for periodNumber in range(1, 5):
         uniqueClasses = studentRecords['Period {} Class'.format(periodNumber)].unique()
         for uniqueClass in uniqueClasses:
-            assignClassInfectionRates(periodNumber, uniqueClass)
+            assignClassInfectionRates('Period {} Class'.format(periodNumber), uniqueClass)
 
     pass
 
