79b0e11c983fadf1708248def1afdff2e0845d00
pruthvichivukula <41833442+pruthvichivukula@users.noreply.github.com>
2021-01-23 12:30:23 -0500

    added ta injest

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ student_records_df = pd.read_excel('./Resources/OEC2021_-_School_Record_Book_.xl
 print(student_records_df)
 teacher_records_df = pd.read_excel('./Resources/OEC2021_-_School_Record_Book_.xlsx', sheet_name='Teacher Records')
 print(teacher_records_df)
+ta_records_df = pd.read_excel('./Resources/OEC2021_-_School_Record_Book_.xlsx', sheet_name='Teaching Assistant Records')
+print(ta_records_df)
 infected_student_list = pd.read_excel('./Resources/OEC2021_-_School_Record_Book_.xlsx', sheet_name='ZBY1 Status')
 print(infected_student_list)
 
